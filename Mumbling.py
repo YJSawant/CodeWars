3483a75299256e8d77fc5d5e39e36536d58a7fdc
Yogesh Sawant <yogeshswnt27@gmail.com>
2019-09-24 14:45:29 -0400

    Codewar
    Mumbling code

---
+++ Mumbling.py
@@ -0,0 +1,15 @@
+#Mumbling
+
+#accum("abcd") -> "A-Bb-Ccc-Dddd"
+#accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+#accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+
+def accum(s):
+    str = ""
+    for i in range(0, len(s)):
+        str += s[i].upper()
+        str += s[i].lower()*i
+        if i != len(s)-1:
+            str += "-"
+    return str
